2ecbdfe4b37a67236e9a50b3fc618c498e9395b4
Isabele Macedo <isabeleeduarda307@gmail.com>
2025-09-29 22:32:41 +0000

    atividade 2 e 3

---
+++ aula18/exercicio02/src/index2.ts
@@ -0,0 +1,29 @@
+//criando uma classe chamada Animal
+class Animal{
+    falar(): string {   //criando um metodo classificado como string
+        return `O animal faz som`;
+    }
+}
+
+//criando as classes cachorro e gato e que herdam de Animal
+class Cachorro extends Animal {
+    falar():string {
+        return `au au`;
+    }
+} 
+
+class Gato extends Animal {
+    falar():string {
+        return `meow meow`;    
+    }
+}
+
+//criando uma const animais e 
+const animais: Animal[] = [new Cachorro(),new Gato,new Animal()]
+
+
+animais.forEach ((animal,i)=>{
+    console.log (`Animais ${i+1}: ${animal.falar()}`);
+});
+
+//{}``=>
